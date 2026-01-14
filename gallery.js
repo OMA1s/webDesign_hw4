b727a76aa942b5b42405ffbc1dd147e69ad8641f
oma1s <oma1s.rasool.khan@gmail.com>
2026-01-13 19:58:49 -0500

    hw4 pageLoad tab index

--- gallery.js
+++ gallery.js
@@ -10,4 +10,13 @@ function upDate(previewPic){
     document.getElementById('image').style.backgroundImage = "url('')";
     document.getElementById('image').textContent = "Hover over an image below to display here.";
 		
-	}
+}
+
+function onPageLoad(){
+    const el = document.getElementsByClassName('preview');
+    console.log("OnPageLoad Function");  
+    for(let i = 0; i < el.length; i++){
+        console.log(el[i]);
+        el[i].setAttribute("tabindex", "0");
+    }
+}
